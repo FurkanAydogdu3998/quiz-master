a2fbd088a36f327f5486aeeec76de3405de0dfa9
Furkan Aydogdu <furkanaydogdu3998@outlook.com>
2024-11-27 14:25:25 +0300

    button states, button sprites

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,15 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     // Start is called before the first frame update
     void Start()
     {
+        GetNextQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetButtonSpritesToDefault() {
+        for(int i = 0; i < answerButtons.Length; i++) {
+            Image imageComponent = answerButtons[i].GetComponent<Image>();
+            imageComponent.sprite = defaultAnswerSprite;
+        }
+    }
+
+    private void SetButtonState(bool state) {
+        for (int i = 0; i < answerButtons.Length; i++) {
+            Button buttonComponent = answerButtons[i].GetComponent<Button>();
+            buttonComponent.interactable = state;
+        }
+    }
+
+    private void GetNextQuestion() {
+        SetButtonState(true);
+        SetButtonSpritesToDefault();
+        DisplayQuestion();
+    }
+
+    private void DisplayQuestion() {
         questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++) {
@@ -21,9 +54,19 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnAnswerSelected(int index) {
+        if (index == question.GetCorrectAnswerIndex()) {
+            Image selectedButtonImage = answerButtons[index].GetComponent<Image>();
+            selectedButtonImage.sprite = correctAnswerSprite;
+
+            questionText.text = "Congratulations!";
+        } else {
+            Image correctButtonImage = answerButtons[question.GetCorrectAnswerIndex()].GetComponent<Image>();
+            correctButtonImage.sprite = correctAnswerSprite;
+
+            questionText.text = "OOOOH NO Correct Answer is: " + question.GetAnswer(question.GetCorrectAnswerIndex());
+        }
+
+        SetButtonState(false);
     }
 }
